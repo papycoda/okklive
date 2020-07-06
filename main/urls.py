d5f2c7cb164e6f860f1105a1c11f9cea8efbaf76
ogunbanwo opeyemi <opeyemi655@gmail.com>
2020-07-06 11:19:10 +0100

    favicon v2

--- main/urls.py
+++ main/urls.py
@@ -21,7 +21,7 @@ from django.views.generic.base import RedirectView
 
 app_name = "main"
 urlpatterns = [
-    path("/", views.homepage, name="homepage"),
+    path("", views.homepage, name="homepage"),
     path("register/", views.register, name ="register"),
     path("logout/",views.logout_request,name = "logout"),
     path("login/",views.login_request,name = "login"),
